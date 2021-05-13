0889ae11ec96174a6a3449f2832b6e940e1ac9b6
Jia Xun <chaijiaxun@gmail.com>
2021-05-13 02:15:09 -0700

    Bitfield testing

--- singapore_map/singapore_map.ino
+++ singapore_map/singapore_map.ino
@@ -24,18 +24,51 @@
 #define BTN_MODE 0
 #define BTN_LIGHT 1
 
-#define STATE_HOLD_OFF 1
-#define STATE_TRANSITION_ON 2
-#define STATE_HOLD_ON 3
-#define STATE_TRANSITION_OFF 4
+#define STATE_HOLD_OFF 0
+#define STATE_TRANSITION_ON 1
+#define STATE_HOLD_ON 2
+#define STATE_TRANSITION_OFF 3
+
+// 
+/*
+Without Bitfields or structs
+Sketch uses 3362 bytes (41%) of program storage space. Maximum is 8192 bytes.
+Global variables use 81 bytes of dynamic memory.
+
+Without Bitfields with structs
+Sketch uses 3442 bytes (42%) of program storage space. Maximum is 8192 bytes.
+Global variables use 85 bytes of dynamic memory.
+
+With bitfields and structs
+Sketch uses 3588 bytes (43%) of program storage space. Maximum is 8192 bytes.
+Global variables use 80 bytes of dynamic memory.
+
+*/
+
+
+struct GlobalState {
+  int lightingState: 2;
+  uint8_t stateTicksTotal: 8;
+  uint8_t stateTicksLeft: 8;
+  
+  uint8_t currentPatternState: 8;
+  // Button states for MODE, BRIGHTNESS
+  bool modeStatePrev: 1;
+  bool modeState: 1;
+  bool lightStatePrev: 1;
+  bool lightState: 1;
+
+  int currentBrightnessLevel: 3;
+  int brightness = 0;
+  float maxBrightness = 1;
+};
 
 
 
-uint8_t lightingState;
-uint8_t stateTicksTotal;
-uint8_t stateTicksLeft;
+GlobalState state;
 
-uint8_t currentPatternState;
+float brightnessLevels[8] = { 1.0f, 0.7f, 0.5f, 0.35f, 0.2f, 0.1f, 0.05f, 0.025f};
+
 
 // The animation will work in 4 states
 // STATE_HOLD_OFF
@@ -72,11 +105,11 @@ class AnimatedPattern {
     nextPattern = _nextPattern;
   }
   void nextState() {
-    currentPatternState++;
-    if (currentPatternState == totalStates) currentPatternState = 0;
+    state.currentPatternState++;
+    if (state.currentPatternState == totalStates) state.currentPatternState = 0;
   }
   uint8_t getState() {
-    return states[currentPatternState];
+    return states[state.currentPatternState];
   }
   uint8_t getTicksOn() {
     return ticksOn;
@@ -99,8 +132,11 @@ AnimatedPattern * animatedPatternCurrent;
 // For example:
 // 0b00000011
 // NSL and EWL are on, while the rest are not.
+// Simple Cycle Pattern
 uint8_t pattern1[] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000};
+// Appear in order of build
 uint8_t pattern2[] = {0b00000001, 0b00000011, 0b00000111, 0b00001111, 0b00011111, 0b00111111};
+// All on
 uint8_t pattern3[] = {0b00111111};
 
 
@@ -122,37 +158,24 @@ void nextPattern() {
   } else {
     animatedPatternCurrent = animatedPatternHead;
   }
-  lightingState = STATE_HOLD_OFF;
-  currentPatternState = 0;
-  stateTicksLeft = stateTicksTotal = animatedPatternCurrent->getTicksOff();
-}
-
-// Button states for MODE, BRIGHTNESS
-bool buttonStatesPrev [2] = {false, false};
-bool buttonStates [2] = {false, false};
-
-float brightnessLevels[8] = { 1.0f, 0.7f, 0.5f, 0.35f, 0.2f, 0.1f, 0.05f, 0.025f};
-int currentBrightnessLevel = 0;
-
-int brightness = 0;
-float maxBrightness = 1;
-
-bool buttonRelease(uint8_t btn) {
-  return !buttonStates[btn] && buttonStatesPrev[btn];
+  state.lightingState = STATE_HOLD_OFF;
+  state.currentPatternState = 0;
+  state.stateTicksLeft = state.stateTicksTotal = animatedPatternCurrent->getTicksOff();
 }
 
 void buttonStatePreLoop() {
   uint16_t input = analogRead(BUTTON_PIN);
   if (input < 10) {
-    buttonStates[BTN_LIGHT] = buttonStates[BTN_MODE] = 0;
+    state.lightState = state.modeState = 0;
   } else {
-    buttonStates[BTN_MODE] = input > 750;
-    buttonStates[BTN_LIGHT] = !buttonStates[BTN_MODE];
+    state.modeState = input > 750;
+    state.lightState = !state.modeState;
   }
 }
 
 void buttonStatePostLoop() {
-  for ( int i = 0; i < 2; i++ ) buttonStatesPrev[i] = buttonStates[i];
+  state.modeStatePrev = state.modeState;
+  state.lightStatePrev = state.lightState;
 }
 
 
@@ -166,6 +189,9 @@ void setup() {
     Serial.begin(9600);
   #endif
 
+  state.modeState = state.modeStatePrev = state.lightState = state.lightStatePrev = false;
+  state.currentBrightnessLevel = 0;
+
   setupAnimatedPatterns();
   nextPattern();
 }
@@ -174,12 +200,12 @@ void loop() {
   // Calculate buttons
   buttonStatePreLoop();
 
-  if (buttonRelease(BTN_LIGHT)) 
-    currentBrightnessLevel ++;
-  if (currentBrightnessLevel >= 8)
-    currentBrightnessLevel = 0;
+  if (!state.lightState && state.lightStatePrev) // Light button is released
+    state.currentBrightnessLevel ++;
+  if (state.currentBrightnessLevel >= 8)
+    state.currentBrightnessLevel = 0;
 
-  if (buttonRelease(BTN_MODE))
+  if (!state.modeState && state.modeStatePrev) // Mode button is released
     nextPattern();
 
   buttonStatePostLoop();
@@ -188,68 +214,68 @@ void loop() {
   // If this state is 0 ticks, it will skip the rest of the loop
   
   float percentage;
-  switch(lightingState) {
+  switch(state.lightingState) {
     case STATE_HOLD_OFF:
-      brightness = 0;
+      state.brightness = 0;
       break;
     case STATE_TRANSITION_ON:
-      percentage = (float)stateTicksLeft / (float)stateTicksTotal;
-      brightness = (1-percentage) * 255.0f;
+      percentage = (float)state.stateTicksLeft / (float)state.stateTicksTotal;
+      state.brightness = (1-percentage) * 255.0f;
       break;
     case STATE_HOLD_ON:
-      brightness = 255;
+      state.brightness = 255;
       break;
     case STATE_TRANSITION_OFF:
-      percentage = (float)stateTicksLeft / (float)stateTicksTotal;
-      brightness = percentage * 255.0f;
+      percentage = (float)state.stateTicksLeft / (float)state.stateTicksTotal;
+      state.brightness = percentage * 255.0f;
       break;
   }
   // State changing
   bool skipLoop = false;
-  if (stateTicksTotal == 0) skipLoop = true;
-  if (stateTicksLeft == 0) {
-    switch(lightingState) {
+  if (state.stateTicksTotal == 0) skipLoop = true;
+  if (state.stateTicksLeft == 0) {
+    switch(state.lightingState) {
       case STATE_HOLD_OFF:
-        lightingState = STATE_TRANSITION_ON;
-        stateTicksTotal = animatedPatternCurrent->getTicksAnimate();
+        state.lightingState = STATE_TRANSITION_ON;
+        state.stateTicksTotal = animatedPatternCurrent->getTicksAnimate();
         break;
       case STATE_TRANSITION_ON:
-        lightingState = STATE_HOLD_ON;
-        stateTicksTotal = animatedPatternCurrent->getTicksOn();
+        state.lightingState = STATE_HOLD_ON;
+        state.stateTicksTotal = animatedPatternCurrent->getTicksOn();
         break;
       case STATE_HOLD_ON:
-        lightingState = STATE_TRANSITION_OFF;
-        stateTicksTotal = animatedPatternCurrent->getTicksAnimate();
+        state.lightingState = STATE_TRANSITION_OFF;
+        state.stateTicksTotal = animatedPatternCurrent->getTicksAnimate();
         break;
       case STATE_TRANSITION_OFF:
-        lightingState = STATE_HOLD_OFF;
-        stateTicksTotal = animatedPatternCurrent->getTicksOff();
+        state.lightingState = STATE_HOLD_OFF;
+        state.stateTicksTotal = animatedPatternCurrent->getTicksOff();
         animatedPatternCurrent->nextState();
         break;
     }
-    stateTicksLeft = stateTicksTotal;
-    if (skipLoop) return; // If the previous state had 0 ticks, we will not update the brightness or lighting.
+    state.stateTicksLeft = state.stateTicksTotal;
+    if (skipLoop) return; // If the previous state had 0 ticks, we will not update the state.brightness or lighting.
   } else {
-    stateTicksLeft--;
+    state.stateTicksLeft--;
   }
-  // Set pin brightness
-  brightness = brightness * brightnessLevels[currentBrightnessLevel];
-  analogWrite(BRIGHTNESS_PIN, 255 - brightness); // Invert the brightness value
+  // Set pin state.brightness
+  state.brightness = state.brightness * brightnessLevels[state.currentBrightnessLevel];
+  analogWrite(BRIGHTNESS_PIN, 255 - state.brightness); // Invert the state.brightness value
 
   // Set the image state
   digitalWrite(SR_LATCH_PIN, LOW);
   shiftOut(SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, animatedPatternCurrent->getState());
   digitalWrite(SR_LATCH_PIN, HIGH);
 
-  // puts("Brightness "); putsln(brightness);
-  // puts(currentPatternState); puts(' ');
+  // puts("Brightness "); putsln(state.brightness);
+  // puts(state.currentPatternState); puts(' ');
   // puts(animatedPatternCurrent->getTicksOn()); puts(' ');
   // puts(animatedPatternCurrent->getTicksAnimate()); puts(' ');
   // puts(animatedPatternCurrent->getTicksOff()); puts(' ');
   // putsln(animatedPatternCurrent->getState());
   // animatedPatternCurrent->getCurrentState()->printState();
-  // puts("State Ticks "); putsln(stateTicksLeft);
-  // puts("Anim State "); putsln(lightingState);
+  // puts("State Ticks "); putsln(state.stateTicksLeft);
+  // puts("Anim State "); putsln(state.lightingState);
 
   // For testing on ATTINY without serial
   // analogWrite(BRIGHTNESS_PIN, (millis() / 10) % 255);
